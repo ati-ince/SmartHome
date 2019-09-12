9ed09cf622fb0358d52600525b322544a84fbb4f
ati <atahir.ince@gmail.com>
2019-09-12 22:49:53 +0300

    [2.5.2]

--- SmartHomeFrameworkV2.1/Logging.cs
+++ SmartHomeFrameworkV2.1/Logging.cs
@@ -6,16 +6,35 @@ using System.Data;
 using System.Text;
 using System.IO;
 
-namespace SmartHomeFrameworkV2._1
+namespace SerialApp
 {
     public class Logging
     {
+        /**/
+        System.IO.StreamWriter file;
+        static string FileName;
+        //
+
+
+        // Log start file burada olusturulacak.. Boylece her yazilimda bir tane dosya olacak.
+        public void LoggingStart()
+        { 
+            String trh = DateTime.Now.ToShortDateString().Replace('/', '.');
+            String zmn = DateTime.Now.ToLongTimeString().Replace(':', '.');
+            //
+            FileName="LogFile" + "_" + trh + "_" + zmn + ".txt";
+        }
+
+        // Dosyaya her log girisi yapilacaginda bu cagirilmakta..
         public void Logging2Txt(string Event, string Comment)
         {
             String trh = DateTime.Now.ToShortDateString().Replace('/', '.');
             String zmn = DateTime.Now.ToLongTimeString().Replace(':', '.');
-            StreamWriter w = File.AppendText("log.txt");
-            w.WriteLine(trh + " " + zmn + " " + Event + " " + Comment);  
+            //
+            file = new System.IO.StreamWriter(FileName, true);
+            file.WriteLine(trh + " " + zmn + " " + Event + " " + Comment);
+            file.Close();
         }
+
     }
 }
